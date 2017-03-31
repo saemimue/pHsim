ccebc290e1a25c261a7295d363f52165b639a2bf
muhn <saemimue@gmail.com>
2017-03-31 17:29:41 +0200

    looks much better. some things can still be improofed

--- model.py
+++ model.py
@@ -2,17 +2,17 @@ from wtforms import Form, StringField, validators
 
 class InputForm(Form):
     c_Ac = StringField(
-            label='c(Ac) [mol/L]', default=0.1,
+            label='c(Ac) [mol/L]', default="2e-5, 3e-5, 1e-5",
             validators=[validators.InputRequired()])
     c_B = StringField(
-            label='c(B) [mol/L]', default=0,
+            label='c(B) [mol/L]', default="1e-5",
             validators=[validators.InputRequired()])
     pKs_Ac = StringField(
-            label='pKs(Ac)', default=-2,
+            label='pKs(Ac)', default="[-1.32],[-3, 1.92],[-6]",
             validators=[validators.InputRequired()])
     pKs_B = StringField(
-            label='pKs(B)', default=14,
+            label='pKs(B)', default="[9.25]",
             validators=[validators.InputRequired()])
     pKs_Tit = StringField(
-            label='pKs(Tit)', default=14,
+            label='pKs(Tit)', default="[14]",
             validators=[validators.InputRequired()])
